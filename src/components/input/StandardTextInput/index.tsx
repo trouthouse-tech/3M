f868ba0d6531bd51370c8a1bfbea38bcb29d168d
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-13 16:34:20 -0400

    Optional props

--- src/components/input/StandardTextInput/index.tsx
+++ src/components/input/StandardTextInput/index.tsx
@@ -1,39 +1,58 @@
 import React from 'react';
 
-import {StyleSheet, TextInput} from 'react-native';
+import {KeyboardType, StyleSheet, TextInput} from 'react-native';
 import {Fonts, Utils} from '../../../styles';
 
 type InputProps = {
-  placeholder: string;
   onChangeText(text: string): void;
   value: string;
-  autoFocus: boolean;
-  secureTextEntry: boolean;
+  autoFocus?: boolean;
+  secureTextEntry?: boolean;
+  placeholder?: string;
+  onBlur?(): void;
+  keyboardType?: KeyboardType;
+  editable?: boolean;
 };
 
 export function StandardTextInput(props: InputProps) {
-  const {placeholder, onChangeText, value, autoFocus, secureTextEntry} = props;
+  const {
+    placeholder,
+    onChangeText,
+    value,
+    autoFocus,
+    secureTextEntry,
+    onBlur,
+    keyboardType,
+    editable,
+  } = props;
 
   return (
     <TextInput
       placeholder={placeholder}
+      placeholderTextColor="rgba(0,0,0,.5)"
       autoCapitalize="none"
       autoFocus={autoFocus || false}
       secureTextEntry={secureTextEntry || false}
       style={styles.textInput}
       onChangeText={(username) => onChangeText(username)}
       value={value}
+      onBlur={() => !onBlur || onBlur()}
+      keyboardType={keyboardType || 'default'}
+      editable={editable || true}
     />
   );
 }
 
 const styles = StyleSheet.create({
   textInput: {
-    ...Fonts.large,
-    borderBottomWidth: 1,
-    borderBottomColor: 'gray',
+    ...Fonts.normal,
+    borderWidth: 1,
+    borderColor: 'gray',
     marginVertical: 10,
+    borderRadius: 10,
     width: Utils.DEVICE_WIDTH / 1.3,
-    height: 60,
+    height: 40,
+    padding: 10,
+    color: 'black',
   },
 });
