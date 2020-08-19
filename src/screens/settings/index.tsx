bbf7818e67bcf4e71f2f25dd270c8e711f1a2369
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-19 19:22:57 -0400

    Added Header and signout

--- src/screens/settings/index.tsx
+++ src/screens/settings/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {AppState} from '../../store/types';
 import {connect} from 'react-redux';
 import {SettingsStackProps} from '../../navigation/Settings/types';
+import Header from '../../components/Header';
+import AsyncStorage from '@react-native-community/async-storage';
+import auth from '@react-native-firebase/auth';
+import {ROUTES} from '../../util/routes';
 
 const SettingsBase = (props: SettingsStackProps) => {
   console.log('user: ', props.user);
-  return <Text>Settings</Text>;
+
+  async function handleSignOut() {
+    await AsyncStorage.setItem('isInvestorLoggedIn', 'false');
+    auth().signOut();
+    props.navigation.reset({
+      index: 0,
+      routes: [{name: ROUTES.Onboarding}],
+    });
+  }
+
+  return (
+    <View>
+      <Header
+        showLogo={true}
+        rightButton={() => handleSignOut()}
+        bottomBorder={true}
+      />
+      <Text>Settings</Text>
+    </View>
+  );
 };
 
 const mapStateToProps = (state: AppState) => ({
