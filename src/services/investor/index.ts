0e6f7c447219d48152811f1ea9da8ce721381a82
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-19 19:47:22 -0400

    Method to update AsyncStorage

--- src/services/investor/index.ts
+++ src/services/investor/index.ts
@@ -3,6 +3,7 @@ import {getDocumentWithPathAndId} from '../../api/firebase/DocumentRetriever';
 import {FIREBASE_PATHS} from '../paths';
 import {Investor} from '../../model';
 import {createDocument} from '../../api/firebase/DocumentCreator';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export async function registerInvestor(email: string, password: string) {
   return await auth()
@@ -40,4 +41,12 @@ export async function createInvestor(investor: Investor) {
     investor.email as string,
     investor,
   );
+}
+
+/**
+ * We need to update AsyncStorage to indicate if a user has logged in. It will be set to false upon logout.
+ * The corresponding key will be used when the application is loaded to help direct the auto-login feature.
+ */
+export function setLoggedInUser() {
+  AsyncStorage.setItem('isInvestorLoggedIn', 'true');
 }
