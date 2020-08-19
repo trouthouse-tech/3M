32d24b72860a29038c59ac108b625d70974a8d0f
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-19 17:20:28 -0400

    Register, Create, Retrieve, and login investor

--- src/services/investor/index.ts
+++ src/services/investor/index.ts
@@ -1,6 +1,10 @@
 import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {getDocumentWithPathAndId} from '../../api/firebase/DocumentRetriever';
+import {FIREBASE_PATHS} from '../paths';
+import {Investor} from '../../model';
+import {createDocument} from '../../api/firebase/DocumentCreator';
 
-export async function createInvestor(email: string, password: string) {
+export async function registerInvestor(email: string, password: string) {
   return await auth()
     .createUserWithEmailAndPassword(email, password)
     .then((user: FirebaseAuthTypes.UserCredential) => {
@@ -11,7 +15,7 @@ export async function createInvestor(email: string, password: string) {
     });
 }
 
-export async function loginInvestor(email: string, password: string) {
+export async function login(email: string, password: string) {
   return await auth()
     .signInWithEmailAndPassword(email, password)
     .then((user: FirebaseAuthTypes.UserCredential) => {
@@ -20,4 +24,20 @@ export async function loginInvestor(email: string, password: string) {
     .catch((error) => {
       return {user: undefined, error: error};
     });
+}
+
+/**
+ * Retrieve Investor's information
+ * @param email
+ */
+export async function getInvestor(email: string) {
+  return await getDocumentWithPathAndId(FIREBASE_PATHS.Investors, email);
+}
+
+export async function createInvestor(investor: Investor) {
+  return await createDocument(
+    FIREBASE_PATHS.Investors,
+    investor.email as string,
+    investor,
+  );
 }
