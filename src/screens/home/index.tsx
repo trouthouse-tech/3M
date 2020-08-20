dffffc2dd1028b06370c2a314451b98df3a18831
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-19 21:04:34 -0400

    Added Header

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {AppState} from '../../store/types';
 import {connect} from 'react-redux';
 import {HomeStackProps} from '../../navigation/home/types';
+import Header from '../../components/Header';
 
 const HomeBase = (props: HomeStackProps) => {
   console.log('user: ', props.user);
-  return <Text>Home</Text>;
+  return (
+    <View>
+      <Header showLogo bottomBorder />
+      <Text>Home</Text>
+    </View>
+  );
 };
 
 const mapStateToProps = (state: AppState) => ({
--- src/screens/trade/index.tsx
+++ src/screens/trade/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {AppState} from '../../store/types';
 import {connect} from 'react-redux';
 import {TradeStackProps} from '../../navigation/trade/types';
+import Header from '../../components/Header';
 
 const TradeBase = (props: TradeStackProps) => {
   console.log('user: ', props.user);
-  return <Text>Trade</Text>;
+  return (
+    <View>
+      <Header showLogo bottomBorder />
+      <Text>Trade</Text>
+    </View>
+  );
 };
 
 const mapStateToProps = (state: AppState) => ({
