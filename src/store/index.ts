d4b36e4ce2eae5f9d2d7329c47e13dffe93732af
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-28 17:00:02 -0400

    Added onboarding reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import {createStore, combineReducers} from 'redux';
 import {AppState} from './types';
 import {user} from './user/reducer';
+import {onboarding} from './onboarding/reducer';
 
 const rootReducer = combineReducers<AppState>({
   user,
+  onboarding,
 });
 
 const store = createStore(rootReducer);
