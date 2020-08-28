797eaa89da731b862c8359943e72ae04909f937f
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-28 16:59:49 -0400

    Added OnboardingQuestionState;

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,5 +1,7 @@
 import {UserState} from './user/types';
+import {OnboardingQuestionState} from './onboarding/types';
 
 export type AppState = {
   user: UserState;
+  onboarding: OnboardingQuestionState;
 };
