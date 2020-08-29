adc7ada2f396a770864b1cd6cee2eeffa0a94839
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-29 12:39:51 -0400

    Added tradier reducer and AppState

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import {createStore, combineReducers} from 'redux';
 import {AppState} from './types';
 import {user} from './user/reducer';
 import {onboarding} from './onboarding/reducer';
+import {tradier} from './tradier/reducer';
 
 const rootReducer = combineReducers<AppState>({
   user,
   onboarding,
+  tradier,
 });
 
 const store = createStore(rootReducer);
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,7 +1,9 @@
 import {UserState} from './user/types';
 import {OnboardingQuestionState} from './onboarding/types';
+import {TradierCredentialsState} from './tradier/types';
 
 export type AppState = {
   user: UserState;
   onboarding: OnboardingQuestionState;
+  tradier: TradierCredentialsState;
 };
