e1088db0c13f8914d57f1785ac6cc58ddfc9d841
mzruiz <matthew.z.ruiz@gmail.com>
2020-08-29 12:43:30 -0400

    Prompt user to authenticate Tradier

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, Text, View} from 'react-native';
 import {AppState} from '../../store/types';
 import {connect} from 'react-redux';
 import {HomeStackProps} from '../../navigation/home/types';
 import Header from '../../components/Header';
+import {ROUTES} from '../../util/routes';
+import {UserState} from '../../store/user/types';
+import {TradierCredentialsState} from '../../store/tradier/types';
 
-const HomeBase = (props: HomeStackProps) => {
-  console.log('user: ', props.user);
+type Props = HomeStackProps & {
+  user: UserState;
+  tradier: TradierCredentialsState;
+};
+
+const HomeBase = (props: Props) => {
+  console.log('props: ', props);
+  useEffect(() => {
+    if (!props.user.hasAuthenticatedTradier) {
+      Alert.alert(
+        "Let's setup your Tradier account.",
+        'The 3M Club has partnered with Tradier to enable trading.',
+        [
+          {text: 'Do this later', onPress: () => {}, style: 'cancel'},
+          {text: 'OK', onPress: () => props.navigation.push(ROUTES.Tradier)},
+          // {text: 'Tradier', onPress: () => goToTradier()},
+        ],
+      );
+    } else {
+      Alert.alert(
+        'Tradier account successfully linked.',
+        "Let's go through a quick walk-through of The 3M Club",
+        [
+          {text: 'Do this later', onPress: () => {}, style: 'cancel'},
+          {text: 'OK', onPress: () => console.log('tutorial')},
+          // {text: 'Tradier', onPress: () => goToTradier()},
+        ],
+      );
+    }
+  });
   return (
     <View>
       <Header showLogo bottomBorder />
@@ -17,6 +48,7 @@ const HomeBase = (props: HomeStackProps) => {
 
 const mapStateToProps = (state: AppState) => ({
   user: state.user,
+  tradier: state.tradier,
 });
 
 const mapDispatchToProps = () => ({});
