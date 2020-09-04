fd909ad1394bca27d9bcddd1c9067b1d4eb6df8c
mzruiz <matthew.z.ruiz@gmail.com>
2020-09-04 18:27:22 -0400

    Removed tradier

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,10 @@ import {createStore, combineReducers} from 'redux';
 import {AppState} from './types';
 import {userReducer} from './user/reducer';
 import {onboarding} from './onboarding/reducer';
-import {tradier} from './tradier/reducer';
 
 const rootReducer = combineReducers<AppState>({
   userReducer,
   onboarding,
-  tradier,
 });
 
 const store = createStore(rootReducer);
--- src/store/types.ts
+++ src/store/types.ts
@@ -1,9 +1,7 @@
 import {UserState} from './user/types';
 import {OnboardingQuestionState} from './onboarding/types';
-import {TradierCredentialsState} from './tradier/types';
 
 export type AppState = {
   userReducer: UserState;
   onboarding: OnboardingQuestionState;
-  tradier: TradierCredentialsState;
 };
