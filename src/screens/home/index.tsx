875ede7caa09632fc4882c358ab8abb8ba66906e
mzruiz <matthew.z.ruiz@gmail.com>
2020-09-04 19:07:07 -0400

    Handle tradier authentication flow. Added 3M discord link

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,48 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Alert, StyleSheet, Text, View, Linking} from 'react-native';
 import {AppState} from '../../store/types';
 import {connect} from 'react-redux';
 import {HomeStackProps} from '../../navigation/home/types';
 import Header from '../../components/Header';
 import {UserState} from '../../store/user/types';
-import {TradierCredentialsState} from '../../store/tradier/types';
 import {Fonts} from '../../styles';
+import {ROUTES} from '../../util/routes';
+import {updateInvestorDocument} from '../../services/investor';
 
 type Props = HomeStackProps & {
   user: UserState;
-  tradier: TradierCredentialsState;
 };
 
 const HomeBase = (props: Props) => {
   console.log('props: ', props);
-  // useEffect(() => {
-  //   if (!props.user.hasAuthenticatedTradier) {
-  //     Alert.alert(
-  //       "Let's setup your Tradier account.",
-  //       'The 3M Club has partnered with Tradier to enable trading.',
-  //       [
-  //         {text: 'Do this later', onPress: () => {}, style: 'cancel'},
-  //         {text: 'OK', onPress: () => props.navigation.push(ROUTES.Tradier)},
-  //         // {text: 'Tradier', onPress: () => goToTradier()},
-  //       ],
-  //     );
-  //   } else {
-  //     Alert.alert(
-  //       'Tradier account successfully linked.',
-  //       "Let's go through a quick walk-through of The 3M Club",
-  //       [
-  //         {text: 'Do this later', onPress: () => {}, style: 'cancel'},
-  //         {text: 'OK', onPress: () => console.log('tutorial')},
-  //         // {text: 'Tradier', onPress: () => goToTradier()},
-  //       ],
-  //     );
-  //   }
-  // });
+  useEffect(() => {
+    const expiration = props.user.tradierAccessTokenExpiration!;
+    const isExpired = expiration < Date.now() / 1000 - 86399;
+    if (isExpired) {
+      console.log('true');
+      Alert.alert(
+        "Let's setup your Tradier account.",
+        'The 3M Club has partnered with Tradier to enable trading.',
+        [
+          {text: 'Do this later', onPress: () => {}, style: 'cancel'},
+          {text: 'OK', onPress: () => props.navigation.push(ROUTES.Tradier)},
+          // {text: 'Tradier', onPress: () => goToTradier()},
+        ],
+      );
+    } else {
+      handleTradierAuthenticated();
+    }
+  });
+
+  function handleTradierAuthenticated() {
+    updateInvestorDocument(props.user.email!, {
+      tradierAccessToken: props.user.tradierAccessToken,
+      tradierAccessTokenExpiration: props.user.tradierAccessTokenExpiration,
+      hasAuthenticatedTradier: true,
+    });
+  }
 
   const rightHeaderIcon = <Text style={styles.chatText}>Chat</Text>;
 
   async function handleChatPressed() {
-    const url = 'https://discord.gg/8sV5Gmz';
+    const url = 'https://discord.gg/SKbm6tN';
     // Checking if the link is supported for links with custom URL scheme.
     const supported = await Linking.canOpenURL(url);
 
@@ -51,7 +54,7 @@ const HomeBase = (props: Props) => {
       // by some browser in the mobile
       await Linking.openURL(url);
     } else {
-      Alert.alert(`Don't know how to open this URL: ${url}`);
+      console.log(`Don't know how to open this URL: ${url}`);
     }
   }
 
@@ -69,8 +72,7 @@ const HomeBase = (props: Props) => {
 };
 
 const mapStateToProps = (state: AppState) => ({
-  user: state.user,
-  tradier: state.tradier,
+  user: state.userReducer,
 });
 
 const mapDispatchToProps = () => ({});
