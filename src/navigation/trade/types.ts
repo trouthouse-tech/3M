fc507c75c4c5c1cca1db5df1a35b99174dbc3af9
mzruiz <matthew.z.ruiz@gmail.com>
2020-10-04 22:44:47 -0400

    lint

--- src/navigation/trade/types.ts
+++ src/navigation/trade/types.ts
@@ -1,7 +1,7 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 import {ROUTES} from '../../util/routes';
 import {RouteProp} from '@react-navigation/native';
-import {Option, Quote} from '../../model';
+import {Quote} from '../../model';
 
 export type TradeStackParamList = {
   Trade: undefined;
