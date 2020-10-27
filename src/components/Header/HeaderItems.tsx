4f48ed47106dbb16774e33b8eb9b0ce4df13361f
mzruiz <matthew.z.ruiz@gmail.com>
2020-10-27 10:23:57 -0400

    Added new logo

--- src/components/Header/HeaderItems.tsx
+++ src/components/Header/HeaderItems.tsx
@@ -1,14 +1,24 @@
-// @ts-ignore
-import Icon from 'react-native-vector-icons/Entypo';
-Icon.loadFont();
 import React from 'react';
 import {Text} from 'react-native';
+// @ts-ignore
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+EntypoIcon.loadFont();
+// @ts-ignore
+import IonIcon from 'react-native-vector-icons/Ionicons';
+import {Colors} from '../../styles';
+IonIcon.loadFont();
 
 export const BackButton = (
-  <Icon name="chevron-thin-left" size={25} color="black" />
+  <EntypoIcon name="chevron-thin-left" size={25} color={Colors.gray} />
 );
 
-export const LogoutButton = <Icon name="log-out" size={25} color="black" />;
+export const BackArrow = (
+  <IonIcon name="ios-arrow-back-outline" size={25} color={Colors.gray} />
+);
+
+export const LogoutButton = (
+  <EntypoIcon name="log-out" size={25} color={Colors.gray} />
+);
 
 export const ChatButton = <Text>Chat</Text>;
 
